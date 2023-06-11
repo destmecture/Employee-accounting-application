6dd8dc836ea8dbda2a657e63f26183d6f8bcb4bc
destmecture <temzzsv@mail.ru>
2023-06-11 18:46:34 +0300

    uncommented class

--- src/main/java/ru/skypro/lessons/springboot/springboot/config/SecurityConfig.java
+++ src/main/java/ru/skypro/lessons/springboot/springboot/config/SecurityConfig.java
@@ -2,21 +2,18 @@ package ru.skypro.lessons.springboot.springboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.sql.DataSource;
+
 
 @EnableWebSecurity
 @Configuration
@@ -28,36 +25,31 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.csrf()
-//                .disable()
-//                .authorizeHttpRequests(this::customizeRequest);
-//        return http.build();
-//    }
-//
-//
-//
-//    private void customizeRequest(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry){
-//        try {
-//            registry.requestMatchers(new AntPathRequestMatcher("/admin/**"))
-//                    .hasAnyRole("ADMIN")
-//                    .requestMatchers(new AntPathRequestMatcher("/user/**"))
-//                    .hasAnyRole("USER")
-//                    .and()
-//                    .formLogin().permitAll()
-//                    .and().logout()
-//                    .logoutUrl("/logout");
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//    }
-//    @Bean
-//    public DaoAuthenticationProvider authProvider() {
-//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-//        authProvider.setUserDetailsService(userDetailsService);
-//        authProvider.setPasswordEncoder(passwordEncoder());
-//        return authProvider;
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeHttpRequests(this::customizeRequest);
+        return http.build();
+    }
+
+
+    private void customizeRequest(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        try {
+            registry.requestMatchers("/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html",
+                            "/swagger-resources/**", "/webjars/**")
+                    .permitAll()
+                    .requestMatchers(new AntPathRequestMatcher("/admin/**"))
+                    .hasAnyRole("ADMIN")
+                    .requestMatchers(new AntPathRequestMatcher("/user/**"))
+                    .hasAnyRole("USER")
+                    .and()
+                    .formLogin().permitAll()
+                    .and().logout()
+                    .logoutUrl("/logout");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
